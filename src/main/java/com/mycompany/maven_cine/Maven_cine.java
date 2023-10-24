356c1ec24a085e522a2312033efba76eb88607ef
cRistianMel0 <118846884+cRistianMel0@users.noreply.github.com>
2023-10-24 17:07:30 -0500

    subir proyeto al repositorio

---
+++ src/main/java/com/mycompany/maven_cine/Maven_cine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maven_cine;
+
+/**
+ *
+ * @author SENA
+ */
+public class Maven_cine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
